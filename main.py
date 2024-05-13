cb30bb146d9e095c8bdfc42c5fb59fc52f1b9aa6
Omar <omarvfais@gmail.com>
2024-05-13 18:20:18 +0530

    bg

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from pygame.locals import *
 
 pygame.init()
 
-SCREEN_WIDTH = 487
-SCREEN_HEIGHT = 548
+SCREEN_WIDTH = 548
+SCREEN_HEIGHT = 487
 
 screen = pygame.display.set_mode((SCREEN_WIDTH ,SCREEN_HEIGHT))
 pygame.display.set_caption('Flappy Bird')
